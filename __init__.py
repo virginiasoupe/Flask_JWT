16a0109d400fd473d8929e7c0020b1db81d3a7a7
bstocker <boris.stocker@gmail.com>
2025-01-27 15:02:15 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+from flask import json
+import sqlite3
+                                                                                                                                       
+app = Flask(__name__)                                                                                                                  
+                                                                                                                                       
+@app.route('/')
+def hello_world():
+    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
+
+@app.route('/exercices/')
+def exercices():
+    return render_template('exercices.html')
+                                                                                                               
+if __name__ == "__main__":
+  app.run(debug=True)
